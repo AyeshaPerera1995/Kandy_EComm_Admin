588c17e3334bba7fb29e07b6ffea4f1ef5cc8841
Ayesha Perera <71303535+AyeshaPerera1995@users.noreply.github.com>
2024-02-18 19:56:17 +0000

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,7 +1,3 @@
 module.exports = {
-   // "baseUrl" : "" //Live URL
-   // "baseUrl" : "http://kandyecommerce-001-site1.atempurl.com/api/ECommerce/Admin"  //Test URL 1
-   // "baseUrl" : "http://ecommercekandy-001-site1.atempurl.com/api/ECommerce/Admin"  //Test URL 2
-   // "baseUrl" : "http://kandyecom-001-site1.itempurl.com/api/ECommerce/Admin"  //Test URL 3
-   "baseUrl" : "http://app360dev-001-site5.dtempurl.com/api/ECommerce/Admin"  //Test URL 4
+   "baseUrl" : ""
 }
